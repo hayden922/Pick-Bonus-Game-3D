c25608255ba91f1d9f860eabdb470cd91fab5574
hayden922 <hayden.franklin922@gmail.com>
2023-11-13 21:36:02 -0500

    multi-chest win-semi-done

--- Assets/Scripts/WinningSolver.cs
+++ Assets/Scripts/WinningSolver.cs
@@ -16,7 +16,9 @@ public class WinningSolver : MonoBehaviour
 
     int multiplierChest = 0;
 
-    AmountsInList[] amountsInChests;
+    //AmountsInList[] amountsInChests;
+
+    List<AmountsInList> amountsInChests;
 
      public class AmountsInList {
            
@@ -100,40 +102,46 @@ public class WinningSolver : MonoBehaviour
     {
         //Method decides win amount and its division amoungst chests.
         int randChestNum = UnityEngine.Random.Range(1,9); //random number of chest to split win amount into. 1 to 8
-        Debug.Log("randChestNum");
-        Debug.Log(randChestNum);
+        
        /// List<float> chestAmountsList = new List<float>(); //list of amounts
         float smallAmount = (denomAmount*multiplier) / 0.05f; //small amount is used to seperate win amount easier. Because each possible num is divisable by 0.05.
-        Debug.Log(denomAmount);
-        Debug.Log(multiplier);
+        
         int currentAmount = System.Convert.ToInt32(smallAmount); //setting small amount to int for use in random range.
+        //float currentAmount = smallAmount;
+        int divisorValue = 0;
         if(multiplier > 5)
         {
             float percent = UnityEngine.Random.Range(1, 101); // 1 to 100 
             if(percent <= 10) //if number is less than or equal to 10 (10% chance)
             {
                 multiplierChest = 3;
+                divisorValue = 8;
+                
             }
             else if(percent <= 25) //if number is less than or equal to 25 (15% chance)
             {
-                multiplierChest = 2;
+                multiplierChest = 3;
+                divisorValue = 8;
             }
 
             else if(percent <= 50) //if number is less than or equal to 50 (25% chance)
             {
-                multiplierChest = 1;
+                multiplierChest = 3;
+                divisorValue = 8;
             }
             else //if number is greater than 50 (50% chance)
             {
-                multiplierChest = 0;
+                multiplierChest = 3;
+                divisorValue = 8;
             }
         }
         else
         {
             multiplierChest = 0;
         }
+        Debug.Log("multiplierChestBelow");
         Debug.Log(multiplierChest);
-        if(randChestNum < multiplierChest)
+        if(randChestNum <= multiplierChest)
         {
             //if there are not enough chests to fit multipliers and win amount.
              switch (multiplierChest) //using switch statment for different multiplier amounts.
@@ -152,10 +160,10 @@ public class WinningSolver : MonoBehaviour
             }
         }
         
-        amountsInChests = new AmountsInList[randChestNum]; //list of amounts
-        for(int i = 0; i <amountsInChests.Length; i++)
+        amountsInChests = new List<AmountsInList>(); //list of amounts
+        for(int i = 0; i < randChestNum; i++)
         {
-          amountsInChests[i] = new AmountsInList(0, false, 0);
+          amountsInChests.Add(new AmountsInList(0, false, 0));
         }
 
        
@@ -166,78 +174,233 @@ public class WinningSolver : MonoBehaviour
        // }
 
 
+       
+
+
+        Debug.Log(randChestNum);
+        for (int i=1; i <= multiplierChest; i++)
+        {
+            int randPos;
+            do
+            {
+                randPos = UnityEngine.Random.Range(0, amountsInChests.Count-1); //Do not want to put multiplier chest as last chest before pooper.
+                
+            }while(amountsInChests[randPos].Multiplier);
+                   
+                Debug.Log(randPos);
+            Debug.Log("multiplierAdded");
+            amountsInChests[randPos] = new AmountsInList(0f,true,0);
+                    
+        }  
+            
+            
+            
         
-        for (int i=0; i < multiplierChest; i++)
-        {
-            bool found = false;
-            while(found == false)
+        int multiplierAmount = divisorValue;
+        Debug.Log(currentAmount);
+        float combinedTotal;
+        do{
+            multiplierAmount = divisorValue;
+            currentAmount = System.Convert.ToInt32(smallAmount);
+            combinedTotal = 0;
+            for (int i = amountsInChests.Count-1; i >= 0; i--) 
             {
-                int randPos = UnityEngine.Random.Range(0, amountsInChests.Length); //Do not want to put multiplier chest as last chest before pooper.   
-                if(!amountsInChests[randPos].Multiplier)
+                    Debug.Log(i);
+                //loops for a number of times equal to the random amount of chests decided -1 so every chest is given a value.
+                if(amountsInChests[i].Multiplier)
                 {
-                    Debug.Log("multiplierAdded");
-                    amountsInChests[randPos] = new AmountsInList(0f,true,0);
-                    found = true;
-                }  
-            }
-            
-            
-        }
-        int multiplierAmount = 0;
-        for (int i = 0; i < amountsInChests.Length-1; i++) 
-        {
-            
-            //loops for a number of times equal to the random amount of chests decided -1 so every chest is given a value.
-            
-            if(amountsInChests[i].Multiplier)
-            {
-                if(multiplierAmount < 2)
-                {
-                    multiplierAmount = 2;
+                    multiplierAmount -= 2;            
                 }
                 else
                 {
-                     multiplierAmount = multiplierAmount*2;
-                }              
-            }
-            else
-            {
-                if(currentAmount > 0) //checker just in case value is so low that it causes 0
-                {
-                    Debug.Log(currentAmount);
-                    int subtractionAmount = UnityEngine.Random.Range(1,((currentAmount+1)/2)); //takes number from 1 to currentAmount/2. Divided by two so there will always be some value left over.
+                    if(currentAmount > 0) //checker just in case value is so low that it causes 0
+                    {
+                        
+                        float total = 0;
+                        int subtractionAmount = 0;
+                        
+                    
+                                
+                        //subtractionAmount = UnityEngine.Random.Range(1,(((currentAmount/multiplierAmount)+1)/2))*multiplierAmount;
+                        
+                        if(multiplierAmount == 0)
+                        {
+                            
+                            subtractionAmount = UnityEngine.Random.Range(1, (currentAmount + 1)); 
+                        }
+                        else
+                        {
+                            
+                            do
+                            {
+                                subtractionAmount = multiplierAmount * UnityEngine.Random.Range(1, ((currentAmount/multiplierAmount)+1));                        
+                            } while (subtractionAmount % multiplierAmount != 0);         
+                        }
 
-           
+                                
+                            //takes number from 1 to currentAmount/2. Divided by two so there will always be some value left over.
 
-           
+                            
+                        total = (subtractionAmount)*0.05f; //turns amount back into dollar amount
+                        Debug.Log(subtractionAmount);
+                        Debug.Log(total);
+                        combinedTotal += total; 
 
-            
-                    float total = subtractionAmount*0.05f; //turns amount back into dollar amount
-
-                   amountsInChests[i] =  new AmountsInList(total,false,multiplierAmount);
-                    Debug.Log("totalAdded");
-                   Debug.Log(amountsInChests[i].Number);
-
-            
-            
-                    currentAmount = currentAmount-subtractionAmount; //takes away amount put in list from current amount
+                        amountsInChests[i] =  new AmountsInList(total,false,multiplierAmount);
+                        
+                
+                        currentAmount = currentAmount-subtractionAmount; //takes away amount put in list from current amount
+                    }
+                    else
+                    {
+                        amountsInChests[i] =  new AmountsInList(0f,false,0);
+                    }
                 }
+                
+                
             }
-            
-            
-        }
+        }while(currentAmount != 0 && (combinedTotal*0.05f) != smallAmount);
+        Debug.Log(currentAmount);
+        Debug.Log("removing");
+        amountsInChests.RemoveAll(item => item.Number == 0 && !item.Multiplier);
 
         
-        float finalAddition = currentAmount*0.05f; //any left over value is the final number added to the list, as long as its not zero.
+        
+        
+        /*
+        
+        float firstAddition;
 
-        if (finalAddition > 0)
+             
+        if(multiplierAmount > 0)
         {
-             amountsInChests[amountsInChests.Length-1] = new AmountsInList(finalAddition,false,multiplierAmount);
+            Debug.Log("multiplier is first");
+            bool foundStorage = false;
+            for(int i = 0;  i < amountsInChests.Length; i++)
+            {
+                if(!amountsInChests[i].Multiplier)
+                {
+                    firstAddition = (currentAmount)*0.05f;
+                    
+                    if((amountsInChests[i].Number + firstAddition)/amountsInChests[i].AppliedMultiplier % 0.05 == 0)
+                    {
+                        amountsInChests[i].Number += firstAddition;
+                        foundStorage = true;
+                        break;         
+                    }
+
+                   
+                }
+            }
+            if(foundStorage == false)
+            {
+                Debug.Log("oops");
+                if(amountsInChests.Length < 8)
+                {
+                    Array.Resize(ref amountsInChests, amountsInChests.Length+1);
+                    Debug.Log(amountsInChests.Length);
+                    for(int i = amountsInChests.Length-1; i > 0; i --)
+                    {
+                        amountsInChests[i] = amountsInChests[i-1];
+                    }
+                    firstAddition = (currentAmount)*0.05f;
+                    amountsInChests[0] = new AmountsInList(firstAddition,false,0);
+                }
+                else
+                {
+                    DecideWinAmounts(denomAmount);
+                }
+                
+
+
+            }
+            
         }
         else
         {
-            Debug.Log("found zero");
+            firstAddition = (currentAmount)*0.05f;
+             amountsInChests[0] = new AmountsInList(firstAddition,false,multiplierAmount);
         }
+
+
+
+        */
+
+
+
+
+        /*
+        else
+        {
+            Debug.Log("found zero");
+            Array.Resize(ref amountsInChests, amountsInChests.Length-1);
+            if(amountsInChests[amountsInChests.Length-1].Multiplier)
+            {
+               Debug.Log("and it caused multiplier to be last");
+               for(int i = amountsInChests.Length-1; i > 0; i--)
+               {
+                    if(!amountsInChests[i].Multiplier && (amountsInChests[i].Number/8f) % 0.05f == 0f && amountsInChests[i].Number !=0 )
+                    {
+                        amountsInChests[i].Multiplier = true;
+                        amountsInChests[amountsInChests.Length].Multiplier = false;
+                        amountsInChests[amountsInChests.Length].Number = amountsInChests[i].Number;
+                        amountsInChests[amountsInChests.Length].AppliedMultiplier = multiplierAmount;
+                        amountsInChests[i].Number = 0f;
+                        amountsInChests[i].AppliedMultiplier = 0;
+
+                    }
+               }
+            }
+        }
+*/
+/*
+        
+        for (int i = 0; i < amountsInChests.Length; i++) 
+        {
+            if(amountsInChests[i].Number/amountsInChests[i].AppliedMultiplier < 0.05f && !amountsInChests[i].Multiplier)
+            {
+                for (int j = 0; j < amountsInChests.Length; j++) 
+                {
+                    if(amountsInChests[j].Number/amountsInChests[i].AppliedMultiplier > 0.05f)
+                    {
+                        Debug.Log("foundLowNum");
+                        Debug.Log(amountsInChests[i].Number);
+                        Debug.Log(amountsInChests[j].Number);
+                        float temp = amountsInChests[i].Number;
+                        amountsInChests[i].Number = amountsInChests[j].Number;
+                        amountsInChests[j].Number = temp;
+                        break;
+
+                    }
+
+                }
+            }
+        }
+/*
+        for (int i = 0; i < amountsInChests.Length; i++) 
+        {
+            if(amountsInChests[i].Number/amountsInChests[i].AppliedMultiplier % 0.05f != 0 && !amountsInChests[i].Multiplier)
+            {
+                for (int j = 0; j < amountsInChests.Length; j++) 
+                {
+                    if(amountsInChests[j].Number/amountsInChests[i].AppliedMultiplier % 0.05f == 0 && !amountsInChests[j].Multiplier)
+                    {
+                        Debug.Log("foundNotDivisable");
+                        Debug.Log(amountsInChests[i].Number);
+                        Debug.Log(amountsInChests[j].Number);
+                        float temp = amountsInChests[i].Number;
+                        amountsInChests[i].Number = amountsInChests[j].Number;
+                        amountsInChests[j].Number = temp;
+                        break;
+
+                    }
+
+                }
+            }
+        }
+
+*/
+
         
 
        /*
@@ -255,15 +418,23 @@ public class WinningSolver : MonoBehaviour
 
         float winAmount = denomAmount*multiplier;
 
-        for (int i = 0; i < amountsInChests.Length; i++) 
+        for (int i = 0; i < amountsInChests.Count; i++) 
         {
-           Debug.Log(amountsInChests[i].Multiplier); 
+            Debug.Log("start"); 
+           Debug.Log(amountsInChests[i].Number); 
+           if(amountsInChests[i].Number == 0f && amountsInChests[i].Multiplier == false)
+           {
+            Debug.Log("still have zeros");
+           }
+           Debug.Log(amountsInChests[i].Multiplier);
+           Debug.Log(amountsInChests[i].AppliedMultiplier);
+           Debug.Log("end"); 
         }
        
-
+        AmountsInList[] amountsArray = amountsInChests.ToArray();
         
 
-        return (winAmount, amountsInChests);
+        return (winAmount, amountsArray);
 
 
 
@@ -275,4 +446,6 @@ public class WinningSolver : MonoBehaviour
 
 
     }
+
+ 
 }
